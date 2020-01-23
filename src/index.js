2fd9748b76fec10204d74492a9217f7be7acf84c
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-23 14:37:58 +0000

    refactored the stats

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({good, neutral, bad}) => {
+
+  const totalScore = good + neutral*0 - bad
+  const totalNumberOfScore = good + neutral + bad
+  const averageScore = totalScore / totalNumberOfScore
+
+  return (
+    <div>
+     <p>Total: {totalScore}</p>
+     <p>Total Number Of Score: {totalNumberOfScore}</p>
+     <p>Total Average: {averageScore}</p>
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const totalScore = good + neutral*0 - bad
-  const totalNumberOfScore = good + neutral + bad
-  const averageScore = totalScore / totalNumberOfScore
 
   return (
     <div>
@@ -25,16 +36,9 @@ const App = () => {
         bad
         </button>
       </div>
-
-      <h1>statistics</h1>
-        <p>good: {good}</p> 
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-
-        <p>Total score: {totalScore}</p>
-        <p>Average score: {averageScore}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
-  )
+  )  
 }
 
 ReactDOM.render(<App />, 
